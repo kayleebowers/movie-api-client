687a94e5ac48a67565f9f2406a7df231ff2e762e
kayleebowers <114454094+kayleebowers@users.noreply.github.com>
2023-07-10 20:50:46 +0000

    feat: add more movie info to MovieView

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -2,18 +2,30 @@ export const MovieView = ({ movie, onBackClick }) => {
   return (
     <div>
       <img src={movie.ImagePath} alt="movie poster" />
-      <div>
+      <p>
+        <span>Title: </span>
+        <span>{movie.Title}</span>
+      </p>
+      <p>
         <span>Description: </span>
         <span>{movie.Description}</span>
-      </div>
-      <div>
+      </p>
+      <p>
         <span>Director: </span>
         <span>{movie.Director.Name}</span>
-      </div>
-      <div>
+      </p>
+      <p>
+        <span>About the director: </span>
+        <span>{movie.Director.Description}</span>
+      </p>
+      <p>
         <span>Genre: </span>
         <span>{movie.Genre.Name}</span>
-      </div>
+      </p>
+      <p>
+        <span>About the genre: </span>
+        <span>{movie.Genre.Description}</span>
+      </p>
       <button onClick={onBackClick}>Back</button>
     </div>
   );
