7e234ff36de05aff861a1811fac9bbb71edb9071
kayleebowers <114454094+kayleebowers@users.noreply.github.com>
2023-07-14 13:01:50 +0000

    feat: add handleSubmit function to LoginView

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -1,12 +1,27 @@
 import React from "react";
 
 export const LoginView = () => {
-    const handleSubmit = () => {
+    const handleSubmit = (event) => {
+        //prevent page from reloading on submit
+        event.preventDefault();
+
+        const data = {
+            access: Username,
+            secret: Password
+        }
+
+        fetch("https://movies-app1-3d6bd65a6f09.herokuapp.com/login", {
+            method: "POST",
+            body: JSON.stringify(data)
+        })
+        // .then((response) => {
+            
+        // })
 
     }
   return (
     <>
-        <form>
+        <form onSubmit={handleSubmit}>
             <label>
                 Username
                 <input type="text" required minLength={"5"}/>
@@ -15,7 +30,7 @@ export const LoginView = () => {
                 Password
                 <input type="password" required/>
             </label>
-            <button type="submit" onClick={handleSubmit}>Submit</button>
+            <button type="submit">Submit</button>
         </form>
     </>
   );
