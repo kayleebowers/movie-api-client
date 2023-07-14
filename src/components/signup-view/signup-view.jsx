495d429c326270e7bb57b6db089a0d69cc8c1959
kayleebowers <114454094+kayleebowers@users.noreply.github.com>
2023-07-14 21:22:55 +0000

    style: add React Boostrap Form to SignUpView

--- src/components/signup-view/signup-view.jsx
+++ src/components/signup-view/signup-view.jsx
@@ -13,66 +13,100 @@ export const SignUpView = () => {
       Username: username,
       Password: password,
       Email: email,
-      Birthday: birthday
-    }
+      Birthday: birthday,
+    };
 
     fetch("https://movies-app1-3d6bd65a6f09.herokuapp.com/users", {
       method: "POST",
       headers: {
-        "Content-Type": "application/json"
+        "Content-Type": "application/json",
       },
-      body: JSON.stringify(data)
+      body: JSON.stringify(data),
     }).then((response) => {
       if (response.ok) {
         alert("You are registered!");
       } else {
         alert("Something went wrong.");
       }
-    })
+    });
   };
 
   return (
     <>
-      <form onSubmit={handleSignUp}>
-        <label>
-          Username*
-          <input
-            type="text"
-            required
-            minLength={"5"}
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-          />
-        </label>
-        <label>
-          Password*
-          <input
-            type="password"
-            required
-            minLength={"5"}
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-        </label>
-        <label>
-          Email*
-          <input
-            type="email"
-            required
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
-        </label>
-        <label>
-          Birthday
-          <input
-            type="date"
-            value={birthday}
-            onChange={(e) => setBirthday(e.target.value)}
-          />
-        </label>
-        <button type="submit">Register</button>
-      </form>
+      <Form>
+        <Form.Group
+          as={Row}
+          className="mb-3"
+          controlId="formHorizontalUsername"
+        >
+          <Form.Label column sm={2}>
+            Username*
+          </Form.Label>
+          <Col sm={10}>
+            <Form.Control
+              type="text"
+              minLength="6"
+              value={username}
+              required
+              onChange={(e) => {
+                setUsername(e.target.value);
+              }}
+            />
+          </Col>
+        </Form.Group>
+        <Form.Group
+          as={Row}
+          className="mb-3"
+          controlId="formHorizontalPassword"
+        >
+          <Form.Label column sm={2}>
+            Password*
+          </Form.Label>
+          <Col sm={10}>
+            <Form.Control
+              type="password"
+              required
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </Col>
+        </Form.Group>
+        <Form.Group as={Row} className="mb-3" controlId="formHorizontalEmail">
+          <Form.Label column sm={2}>
+            Email*
+          </Form.Label>
+          <Col sm={10}>
+            <Form.Control
+              type="email"
+              value={email}
+              required
+              minLength="6"
+              onChange={(e) => {
+                setEmail(e.target.value);
+              }}
+            />
+          </Col>
+        </Form.Group>
+        <Form.Group
+          as={Row}
+          className="mb-3"
+          controlId="formHorizontalBirthday"
+        >
+          <Form.Label column sm={2}>
+            Birthday
+          </Form.Label>
+          <Col sm={10}>
+            <Form.Control
+              type="date"
+              value={birthday}
+              onChange={(e) => setBirthday(e.target.value)}
+            />
+          </Col>
+        </Form.Group>
+        <Button type="submit" onClick={handleSubmit}>
+          Submit
+        </Button>
+      </Form>
     </>
   );
 };
