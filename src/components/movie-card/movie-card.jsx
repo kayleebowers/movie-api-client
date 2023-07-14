2b960cf833206f097eda74ed4e496b884678eb53
kayleebowers <114454094+kayleebowers@users.noreply.github.com>
2023-07-14 19:34:31 +0000

    style: add Card style to MovieCard

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,15 +1,21 @@
 //import prop-types module
 import PropTypes from "prop-types";
+import { Button, Card } from "react-bootstrap";
 
 export const MovieCard = ({ movie, onMovieClick }) => {
   return (
-    <div 
+    <Card 
+        style={{ width: '18rem' }}
         onClick = {() => {
             onMovieClick(movie);
         }}
     >
-        {movie.Title}
-    </div>
+        <Card.Img variant="top" src={movie.ImagePath} />
+        <Card.Title>{movie.Title}</Card.Title>
+        <Card.Body>
+            <Button>Learn more</Button>
+        </Card.Body>
+    </Card>
     );
 };
 
