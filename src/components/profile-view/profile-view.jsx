70c4607b3073e38ab2f382a682e6234d3346a0bc
kayleebowers <114454094+kayleebowers@users.noreply.github.com>
2023-07-18 17:42:24 +0000

    feat: get currentUser from users in profile view

--- src/components/profile-view/profile-view.jsx
+++ src/components/profile-view/profile-view.jsx
@@ -2,10 +2,16 @@ import { ProfileInformation } from "./profile-information/profile-information";
 import { ProfileUpdate } from "./profile-update/profile-update";
 import { FavoriteMovies } from "./favorite-movies/favorite-movies";
 
-export const ProfileView = () => {
+import { useParams } from "react-router";
+
+export const ProfileView = ({user}) => {
+    const { username } = useParams();
+
+    const currentUser = user.find((u) => u.Username = username);
+
     return (
         <>
-            <ProfileInformation />
+            <ProfileInformation currentUser={currentUser} />
             <ProfileUpdate />
             <FavoriteMovies />
         </>
