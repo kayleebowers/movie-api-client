da53aaf72b5df0abe8be850bd022cfe324fd9c16
kayleebowers <114454094+kayleebowers@users.noreply.github.com>
2023-07-18 16:49:07 +0000

    style: move login and register navlinks to right

--- src/components/navigation-bar/navigation-bar.jsx
+++ src/components/navigation-bar/navigation-bar.jsx
@@ -1,6 +1,7 @@
 import Container from "react-bootstrap/Container";
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
+// import { Container, Button, Row, Col, Card } from "react-bootstrap";
 import { Link } from "react-router-dom";
 
 export const NavigationBar = ({ user, onLoggedOut }) => {
@@ -20,19 +21,25 @@ export const NavigationBar = ({ user, onLoggedOut }) => {
               <Nav.Link as={Link} to="/">
                 Profile
               </Nav.Link>
+              <Nav className="ms-auto">
               <Nav.Link as={Link} to="/login" onClick={onLoggedOut}>
                 Logout
               </Nav.Link>
+              </Nav>
             </>
           )}
+        </Nav>
+        <Nav className="ms-auto">
           {!user && (
             <>
-              <Nav.Link as={Link} to="/">
-                Login
-              </Nav.Link>
-              <Nav.Link as={Link} to="/users">
-                Register
-              </Nav.Link>
+                <Navbar.Collapse className="justify-content-end">
+                <Nav.Link as={Link} to="/">
+                  Login
+                </Nav.Link>
+                <Nav.Link as={Link} to="/users">
+                  Register
+                </Nav.Link>
+                </Navbar.Collapse>
             </>
           )}
         </Nav>
