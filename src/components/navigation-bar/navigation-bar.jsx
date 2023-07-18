044554ba22a2af8bd628364a9394795fd73f26c7
kayleebowers <114454094+kayleebowers@users.noreply.github.com>
2023-07-18 16:41:07 +0000

    feat: add different Nav.Links to user/no user

--- src/components/navigation-bar/navigation-bar.jsx
+++ src/components/navigation-bar/navigation-bar.jsx
@@ -1,6 +1,6 @@
 import Container from "react-bootstrap/Container";
 import Navbar from "react-bootstrap/Navbar";
-import Nav from 'react-bootstrap/Nav';
+import Nav from "react-bootstrap/Nav";
 import { Link } from "react-router-dom";
 
 export const NavigationBar = ({ user, onLoggedOut }) => {
@@ -10,16 +10,32 @@ export const NavigationBar = ({ user, onLoggedOut }) => {
         <Navbar.Brand as={Link} to="/">
           myFlix
         </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Nav className="me-auto">
-            <Nav.Link as={Link} to="/">Home</Nav.Link>
-            <Nav.Link as={Link} to="/">Profile</Nav.Link>
-          </Nav>
-        <Navbar.Toggle />
-        <Navbar.Collapse className="justify-content-end">
-          <Navbar.Text as={Link} to="/login" onClick={onLoggedOut}>
-            Logout
-          </Navbar.Text>
-        </Navbar.Collapse>
+          {user && (
+            <>
+              <Nav.Link as={Link} to="/">
+                Home
+              </Nav.Link>
+              <Nav.Link as={Link} to="/">
+                Profile
+              </Nav.Link>
+              <Nav.Link as={Link} to="/login" onClick={onLoggedOut}>
+                Logout
+              </Nav.Link>
+            </>
+          )}
+          {!user && (
+            <>
+              <Nav.Link as={Link} to="/">
+                Login
+              </Nav.Link>
+              <Nav.Link as={Link} to="/users">
+                Register
+              </Nav.Link>
+            </>
+          )}
+        </Nav>
       </Container>
     </Navbar>
   );
