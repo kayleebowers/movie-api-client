d3b2f02066b1c1e8f8d35cb8d6c51b0a4257b73d
kayleebowers <114454094+kayleebowers@users.noreply.github.com>
2023-07-18 18:11:15 +0000

    feat: add api call to get user data

--- src/components/profile-view/profile-information/profile-information.jsx
+++ src/components/profile-view/profile-information/profile-information.jsx
@@ -1,5 +1,5 @@
 export const ProfileInformation = ({currentUser, token}) => {
-
+    
     return (
         <>
         </>
--- src/components/profile-view/profile-view.jsx
+++ src/components/profile-view/profile-view.jsx
@@ -4,10 +4,22 @@ import { FavoriteMovies } from "./favorite-movies/favorite-movies";
 
 import { useParams } from "react-router";
 
-export const ProfileView = ({user}) => {
+export const ProfileView = ({user, token}) => {
     const { username } = useParams();
-
     const currentUser = user.find((u) => u.Username = username);
+
+    //get current user data from users API
+    fetch(`https://movies-app1-3d6bd65a6f09.herokuapp.com/users/${currentUser}`, {
+        headers: { Authorization: `Bearer ${token}`}
+    }).then((response) => response.json())
+    .then((user) => {
+        return {
+            Username: user.Username,
+            Email: user.Email,
+            Birthday: user.Birthday,
+            Favorites: user.Favorites
+        }
+    })
 
     return (
         <>
