377f6c045a3dde2b24d802f3fd243047fe7223d4
kayleebowers <114454094+kayleebowers@users.noreply.github.com>
2023-07-18 14:57:04 +0000

    feat: refactor MovieCard to use route Link

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,21 +1,18 @@
 //import prop-types module
+import React from "react";
 import PropTypes from "prop-types";
 import { Button, Card } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
-export const MovieCard = ({ movie, onMovieClick }) => {
+export const MovieCard = ({ movie }) => {
   return (
-    <Card 
-        className="h-100"
-        style={{ width: '18rem' }}
-        onClick = {() => {
-            onMovieClick(movie);
-        }}
-    >
+    <Card className="h-100" style={{ width: '18rem' }} >
         <Card.Img className="img-fluid h-75" variant="top" src={movie.ImagePath} />
         <Card.Title className="mx-auto my-4">{movie.Title}</Card.Title>
         <Card.Body>
-            <Button className="float-start">Learn more</Button>
-            <Button className="float-end">Add to favorites</Button>
+            <Link to={`/movies/${movie._id}`}>
+                <Button variant="link">Learn more</Button>
+            </Link>
         </Card.Body>
     </Card>
     );
@@ -25,6 +22,5 @@ export const MovieCard = ({ movie, onMovieClick }) => {
 MovieCard.propTypes = {
     movie: PropTypes.shape({
         Title: PropTypes.string
-    }).isRequired,
-    onMovieClick: PropTypes.func.isRequired 
+    }).isRequired
 };
