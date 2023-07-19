f4e3e0cb590a946fa5bd0314877605ba7d12967d
kayleebowers <114454094+kayleebowers@users.noreply.github.com>
2023-07-19 14:39:44 +0000

    feat: add animated heart button to movie card

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -3,11 +3,15 @@ import React from "react";
 import PropTypes from "prop-types";
 import { Button, Card } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import Heart from "react-animated-heart";
+import { useState } from "react";
 
 export const MovieCard = ({ movie }) => {
+    const [isClick, setClick] = useState(false);
   return (
-    <Card className="h-100" style={{ width: '18rem' }} >
+    <Card className="h-100" style={{ width: '22rem' }} >
         <Card.Img className="img-fluid h-75" variant="top" src={movie.ImagePath} />
+        <Heart isClick={isClick} onClick={() => setClick(!isClick)} />
         <Card.Title className="mx-auto my-4">{movie.Title}</Card.Title>
         <Card.Body>
             <Link to={`/movies/${encodeURIComponent(movie.Title)}`}>
