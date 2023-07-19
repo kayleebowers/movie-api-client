fded9c8f40d87b06f5f11f7006e78c824942f557
kayleebowers <114454094+kayleebowers@users.noreply.github.com>
2023-07-19 21:52:18 +0000

    fix: add useEffect hook to profile view

--- src/components/profile-view/favorite-movies/favorite-movies.jsx
+++ src/components/profile-view/favorite-movies/favorite-movies.jsx
@@ -1,18 +1,25 @@
 import { Container, Button, Row, Col, Card } from "react-bootstrap";
 import { MovieCard } from "../../movie-card/movie-card";
+import { Col, Row, Button, Card } from "react-bootstrap";
 
 export const FavoriteMovies = ({ favoriteMovies, movie }) => {
   return (
     <>
       <h2>Your favorite movies :(</h2>
-      <p>Why doesn't it work?</p>
+      <Row>
         { favoriteMovies && (
           favoriteMovies.map((movie) => {
-          <Col xs={12} s={6} md={3} key={movie._id}>
-            <MovieCard movie={movie} />
-          </Col>
+            return (
+              <Col xs={12} s={6} md={3} key={movie._id}>
+                <MovieCard movie={movie} />
+              </Col>
+            )
         })
         )}
+        { !favoriteMovies && (
+          <Col>No favorite movies yet</Col>
+        )}
+      </Row>
     </>
   );
 };
--- src/components/profile-view/profile-view.jsx
+++ src/components/profile-view/profile-view.jsx
@@ -1,45 +1,55 @@
 import { ProfileInformation } from "./profile-information/profile-information";
 import { ProfileUpdate } from "./profile-update/profile-update";
 import { FavoriteMovies } from "./favorite-movies/favorite-movies";
+import { useEffect } from "react";
 
 import { useParams } from "react-router";
 
-export const ProfileView = ({user, token, setUser, setToken, onLoggedOut, movies }) => {
-    const { id } = useParams();
+export const ProfileView = ({
+  user,
+  token,
+  setUser,
+  setToken,
+  onLoggedOut,
+  movies,
+}) => {
+  const { id } = useParams();
 
-    //get favoriteMovies array
-    let favoriteMovies = user.Favorites;
+  //get favoriteMovies array
+  let favoriteMovies = movies.filter((movie) => {
+    return user.Favorites.includes(movie._id);
+  });
 
-    favoriteMovies = movies.filter((movie) => {
-        if (!favoriteMovies) {
-            console.log("No favorite movies");
-        } else {
-            return user.Favorites.includes(movie._id);
-        }
-    });
-
-    //get current user data from users API
+  //get current user data from users API
+  useEffect(() => {
     fetch(`https://movies-app1-3d6bd65a6f09.herokuapp.com/users/${id}`, {
-        headers: { Authorization: `Bearer ${token}`}
-    }).then((response) => response.json())
-    .then((user) => {
+      headers: { Authorization: `Bearer ${token}` },
+    })
+      .then((response) => response.json())
+      .then((user) => {
         return {
-            id: user._id,
-            Username: user.Username,
-            Email: user.Email,
-            Birthday: user.Birthday,
-            Favorites: user.Favorites
-        }
-    })
-    .catch((error) => {
+          id: user._id,
+          Username: user.Username,
+          Email: user.Email,
+          Birthday: user.Birthday,
+          Favorites: user.Favorites,
+        };
+      })
+      .catch((error) => {
         console.error(error);
-    })
+      });
+  }, []);
 
-    return (
-        <>
-            <ProfileInformation user={user} token={token} onLoggedOut={onLoggedOut}/>
-            <ProfileUpdate user={user} token={token} setUser={setUser} setToken={setToken}/>
-            <FavoriteMovies favoriteMovies={favoriteMovies} movies={movies}/>
-        </>
-    )
-}
+  return (
+    <>
+      <ProfileInformation user={user} token={token} onLoggedOut={onLoggedOut} />
+      <ProfileUpdate
+        user={user}
+        token={token}
+        setUser={setUser}
+        setToken={setToken}
+      />
+      <FavoriteMovies favoriteMovies={favoriteMovies} movies={movies} />
+    </>
+  );
+};
