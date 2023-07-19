fa008f7257f14e2f6c6ffc9eda9b66efcae3bb9a
kayleebowers <114454094+kayleebowers@users.noreply.github.com>
2023-07-19 15:44:16 +0000

    fix: simplify favoriteMovies map

--- src/components/profile-view/favorite-movies/favorite-movies.jsx
+++ src/components/profile-view/favorite-movies/favorite-movies.jsx
@@ -4,19 +4,14 @@ import { MovieCard } from "../../movie-card/movie-card";
 export const FavoriteMovies = ({ favoriteMovies }) => {
   return (
     <>
-      {favoriteMovies ? (
-        <>
-          {favoriteMovies.map((movie) => {
-            return (
-              <Col xs={12} s={6} md={3} key={movie._id}>
-                <MovieCard movie={movie} />
-              </Col>
-            );
-          })}
-        </>
-      ) : (
-        <Col>You have no favorite movies</Col>
-      )}
+      <h2>Your favorite movies</h2>
+      {favoriteMovies.map((movie) => {
+        return (
+          <Col xs={12} s={6} md={3} key={movie._id}>
+            <MovieCard movie={movie} />
+          </Col>
+        );
+      })}
     </>
   );
 };
