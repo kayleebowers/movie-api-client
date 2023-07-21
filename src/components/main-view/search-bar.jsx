0f2331e6ac9b7760ae5acd68e3295a99b505dc51
kayleebowers <114454094+kayleebowers@users.noreply.github.com>
2023-07-21 18:33:13 +0000

    style: resize search bar

--- src/components/main-view/search-bar.jsx
+++ src/components/main-view/search-bar.jsx
@@ -2,23 +2,33 @@ import { Button, Row, Col, Card, Nav, Form } from "react-bootstrap";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
-export const SearchBar = ({movies, user, favorites, token, setUser}) => {
-    const [search, setSearch] = useState("");
-    const movie = movies.find((movie) => movie.Title.toUpperCase() === search.toUpperCase());
-    const navigate = useNavigate();
+export const SearchBar = ({ movies, user, favorites, token, setUser }) => {
+  const [search, setSearch] = useState("");
+  const movie = movies.find(
+    (movie) => movie.Title.toUpperCase() === search.toUpperCase()
+  );
 
-    const handleSearch = (event) => {
-        event.preventDefault();
-        if (movie) {
-            navigate(`/movies/${movie.Title}`);
-        } else {
-            alert("No movie found");
-        }
+  const navigate = useNavigate();
+
+  //navigate to MainView for movie if there's a match
+  const handleSearch = (event) => {
+    event.preventDefault();
+    if (movie) {
+      navigate(`/movies/${movie.Title}`);
+    } else {
+      alert("No movie found");
     }
+  };
   return (
     <Row>
-      <Form onSubmit={handleSearch}>
-        <Form.Control type="search" placeholder="Search by title" value={search} onChange={(e) => setSearch(e.target.value)} />
+      <Form onSubmit={handleSearch} className="d-flex justify-content-end mt-4">
+        <Form.Control
+          type="search"
+          placeholder="Search by title"
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+          className="w-50"
+        />
         <Button type="submit">Search</Button>
       </Form>
     </Row>
