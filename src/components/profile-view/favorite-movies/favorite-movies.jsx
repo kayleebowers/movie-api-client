9adb63c8ab35112bed6455dee4ca5cea4cc06f55
kayleebowers <114454094+kayleebowers@users.noreply.github.com>
2023-07-21 12:49:30 +0000

    fix: display favoriteMovies on ProfileView

--- src/components/profile-view/favorite-movies/favorite-movies.jsx
+++ src/components/profile-view/favorite-movies/favorite-movies.jsx
@@ -4,31 +4,23 @@ import { Col, Row, Button, Card } from "react-bootstrap";
 
 export const FavoriteMovies = ({ user, movies }) => {
   //get favoriteMovies array
-  const favoriteMovies = [];
-  movies.forEach((movie) => {
-    for (let i = 0; i < user.Favorites.length; i++) {
-      if (movie._id === user.Favorites[i]) {
-        favoriteMovies.push(movie);
-      }
-    }
-  });
-
-  console.log(favoriteMovies);
+  const fav = user.Favorites;
+  let favoriteMovies = movies.filter(m => fav.includes(m.id));
 
   return (
     <>
       <h2>Your favorite movies</h2>
       <Row>
-        { user.Favorites.map((movie) => {
+        { favoriteMovies.map((movie) => {
             return (
-              <Col>{movie}</Col>
-              // <Col xs={6} md={4} key={movie._id}>
-              //   <MovieCard movie={movie} />
-              // </Col>
+              // <Col>{movie}</Col>
+              <Col xs={6} md={4} key={movie.id}>
+                <MovieCard movie={movie} />
+              </Col>
             )
         })
         }
-        { !user.Favorites && (
+        { !favoriteMovies && (
           <Col>No favorite movies yet</Col>
         )}
       </Row>
