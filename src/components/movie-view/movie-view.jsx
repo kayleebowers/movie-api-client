177e883034b090fa3fffab24a9953e23f66764b6
kayleebowers <114454094+kayleebowers@users.noreply.github.com>
2023-07-21 22:17:07 +0000

    style: add responsive sizing to movieview

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -13,27 +13,27 @@ export const MovieView = ({ movies, user, favorites, token, setUser }) => {
 
   return (
     <>
-      <Card className="mb-4 d-flex justify-content-between border-0 bg-light mt-5">
-        <Row className="d-flex justify-content-between">
-        <Col md={3}>
-          <Card.Img className="mw-60 my-4 mx-3" src={movie.ImagePath} alt="movie poster" />
+      <Card className="mb-4 d-flex border-0 bg-light mt-5">
+        <Row className="d-flex justify-content-center align-content-center bg-light movieView">
+        <Col lg={4} className="mx auto">
+          <Card.Img className="mh-60 p-4" src={movie.ImagePath} alt="movie poster" />
         </Col>
-        <Col md={9}>
-          <Card.Body className="my-4 bg-light">
-            <Card.Title>{movie.Title}</Card.Title>
+        <Col lg={8}>
+          <Card.Body className="my-4 bg-light text-xs-center">
+            <Card.Title className="mb-4 font-weight-bold">{movie.Title}</Card.Title>
             <Card.Text>Description: {movie.Description}</Card.Text>
             <Card.Text>Director: {movie.Director.Name}</Card.Text>
             <Card.Text>About the director: {movie.Director.Bio}</Card.Text>
             <Card.Text>Genre: {movie.Genre.Name}</Card.Text>
             <Card.Text>About the genre: {movie.Genre.Description}</Card.Text>
           </Card.Body>
+          <Card.Footer className="mt-4 bg-light d-flex justify-content-center">
+            <Link to={'/'} className="mt-4">
+              <Button className="mb-3">Back</Button>
+            </Link>
+          </Card.Footer>
         </Col>
         </Row>
-        <Col className="mt-4 mx-auto mr-4">
-          <Link to={'/'}>
-            <Button className="mb-3">Back</Button>
-          </Link>
-        </Col>
       </Card>
       <SimilarMovies movie={movie} movies={movies} user={user} favorites={favorites} token={token} setUser={setUser} />
     </>
