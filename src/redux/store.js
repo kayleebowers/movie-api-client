83d1205c721f76205eb14b7b817994f157cef3e1
kayleebowers <114454094+kayleebowers@users.noreply.github.com>
2023-08-25 20:51:01 +0000

    feat: add moviesReducer to store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
+import moviesReducer from "./reducers/movies";
 
 export default configureStore({
     reducer: {
-        
+        movies: moviesReducer
     }
 })
