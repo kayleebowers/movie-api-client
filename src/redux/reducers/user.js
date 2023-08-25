a46d600498622be6059ba7dff0b3bc764f412564
kayleebowers <114454094+kayleebowers@users.noreply.github.com>
2023-08-25 21:31:33 +0000

    feat: add user reducer with createSlice

---
+++ src/redux/reducers/user.js
@@ -0,0 +1,23 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const userSlice = createSlice({
+    name: "user",
+    initialState: {
+        user: storedUser ? JSON.parse(storedUser) : null,
+        token: storedToken ? storedToken : null
+    },
+    reducers: {
+        setUser: (state, action) => {
+            state.user = action.payload;
+        },
+        setToken: (state, action) => {
+            state.token = action.payload;
+        }
+    }
+});
+
+// export action creators from slice's actions object
+export const { setUser, setToken } = userSlice.actions;
+
+// export reducer
+export default userSlice.reducer;
